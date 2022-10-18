f9220dac555e1744204d886701f0784df2cc75b1
tostyk <ostyk47@gmail.com>
2022-10-19 00:41:59 +0300

    Add generators dll

---
+++ RandomDateTime/RandomDateTime.cs
@@ -0,0 +1,14 @@
+﻿namespace Faker.Generators
+{
+    public class RandomDateTime : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(DateTime);
+        }
+        public object Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            return new DateTime(context.Random.NextInt64(DateTime.MinValue.Ticks, DateTime.MaxValue.Ticks));
+        }
+    }
+}
---
+++ RandomList/RandomList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace Faker.Generators
+{
+    public class RandomList : IValueGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                Type E = type.GetGenericTypeDefinition();
+                return E == typeof(List<>);
+            }
+            return false;
+        }
+        public object? Generate(Type typeToGenerate, GeneratorContext context)
+        {
+            int size = context.Random.Next(0, 20);
+            IList? list = (IList?)Activator.CreateInstance(typeToGenerate);
+            Type genericType = typeToGenerate.GenericTypeArguments[0];
+            for (int i = 0; i < size; i++)
+            {
+                var el = context.Faker.Create(genericType);
+                if (el != null)
+                {
+                    list?.Add(context.Faker.Create(genericType));
+                }
+            }
+            return list;
+        }
+    }
+}
